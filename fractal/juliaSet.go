c01cfd93abffef41f5cbd006266b94c773ad77d1
Andrey Astakhov <andrey.astakhov@gmail.com>
2021-09-14 14:47:24 +0200

    Extract methods.

--- fractal/juliaSet.go
+++ fractal/juliaSet.go
@@ -1,7 +1,7 @@
 package fractal
 
 type FloatPoint struct {
-	X, Y float64
+	X, Y float32
 }
 
 type JuliaSet struct {
@@ -13,37 +13,24 @@ type JuliaSet struct {
 	FocalPoint    FloatPoint
 }
 
-func (fractal JuliaSet) Render(items chan SpaceItem) {
-
-	limit := fractal.EscapeRadius * fractal.EscapeRadius
-
+func (fractal JuliaSet) Render(items chan Element) {
 	var iteration int
 	var x, y float32
 
-	widthScale := fractal.Scale
-	heightScale := fractal.Scale * float32(fractal.Size.Width) / float32(fractal.Size.Height)
-
-	if fractal.Size.Width > fractal.Size.Height {
-		heightScale = 1 / heightScale
-	}
+	widthScale, heightScale := fractal.sizeScales()
 
 	for i := 0; i < fractal.Size.Width; i++ {
 		for j := 0; j < fractal.Size.Height; j++ {
 			iteration = 0
-			x = interpolate(-widthScale, widthScale, i+int(fractal.FocalPoint.X), fractal.Size.Width)    //- config.FocalPoint.X
-			y = interpolate(-heightScale, heightScale, j+int(fractal.FocalPoint.Y), fractal.Size.Height) //- config.FocalPoint.Y
 
-			for x*x+y*y < limit && iteration <= fractal.MaxIterations {
+			x, y = fractal.scaleToSize(i, j, widthScale, heightScale)
+
+			for x*x+y*y < fractal.EscapeRadius*fractal.EscapeRadius && iteration <= fractal.MaxIterations {
 				x, y = x*x-y*y+real(fractal.Complex), 2*x*y+imag(fractal.Complex)
-
 				iteration++
 			}
 
-			if iteration > fractal.MaxIterations {
-				continue
-			}
-
-			items <- SpaceItem{i, j, float32(iteration) / float32(fractal.MaxIterations)}
+			items <- Element{i, j, float32(iteration) / float32(fractal.MaxIterations)}
 		}
 	}
 
@@ -52,4 +39,19 @@ func (fractal JuliaSet) Render(items chan SpaceItem) {
 
 func interpolate(start, end float32, position, size int) float32 {
 	return start + float32(position)*(end-start)/float32(size)
+}
+
+func (fractal JuliaSet) scaleToSize(i, j int, widthScale, heightScale float32) (float32, float32) {
+	return interpolate(-widthScale, widthScale, i+int(fractal.FocalPoint.X), fractal.Size.Width),
+		interpolate(-heightScale, heightScale, j+int(fractal.FocalPoint.Y), fractal.Size.Height)
+}
+
+func (fractal JuliaSet) sizeScales() (float32, float32) {
+	if fractal.Size.Width < fractal.Size.Height {
+		return fractal.Scale,
+			fractal.Scale * float32(fractal.Size.Width) / float32(fractal.Size.Height)
+	}
+
+	return fractal.Scale,
+		fractal.Scale * float32(fractal.Size.Height) / float32(fractal.Size.Width)
 }
