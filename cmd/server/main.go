d2dd9036b68d2d61a2597b72add8e28095291cc0
Andrey Astakhov <andrey.astakhov@gmail.com>
2021-09-18 17:52:09 +0200

    Implement burningship.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,7 +6,7 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/a-ast/go-fractal/pkg/colourpicker"
+	cp "github.com/a-ast/go-fractal/pkg/colourpicker"
 	"github.com/a-ast/go-fractal/pkg/fractals"
 )
 
@@ -17,7 +17,7 @@ func main() {
 func getFractal(w http.ResponseWriter, r *http.Request) {
 	query := NewQuery(r.URL.Query())
 
-	kind := query.GetString("t", "julia")
+	kind := query.GetString("t", "burningship")
 	width := query.GetInt("w", 800)
 	height := query.GetInt("h", 400)
 	zoom := query.GetFloat("z", 1.0)
@@ -30,9 +30,16 @@ func getFractal(w http.ResponseWriter, r *http.Request) {
 
 	if query.Error() != "" {
 		http.Error(w, query.Error(), 404)
+		return
 	}
 
-	colourPicker := colourpicker.Electro
+	colourPicker := cp.NewGradientPicker(1000,
+		[]cp.GradientPoint{
+			{0, cp.Colour{0, 0, 139}},
+			{15, cp.Colour{255, 174, 0}},
+			{25, cp.Colour{174, 174, 100}},
+		},
+	)
 	canvas := fractals.Canvas{
 		Size:   fractals.Size{width, height},
 		Zoom:   zoom,
@@ -51,6 +58,13 @@ func getFractal(w http.ResponseWriter, r *http.Request) {
 		}
 	case "mandelbrot":
 		fractal = fractals.MandelbrotSet{
+			Canvas:        canvas,
+			EscapeRadius:  er,
+			MaxIterations: mi,
+		}
+	// z=5&cx=-1.8&mi=200&er=2
+	case "burningship":
+		fractal = fractals.BurningShip{
 			Canvas:        canvas,
 			EscapeRadius:  er,
 			MaxIterations: mi,
---
+++ pkg/fractals/burningship.go
@@ -0,0 +1,40 @@
+package fractals
+
+import "math"
+
+type BurningShip struct {
+	Canvas        Canvas
+	Complex       complex64
+	EscapeRadius  float32
+	MaxIterations int
+	Center        FloatPoint
+}
+
+func (fractal BurningShip) Render(items chan Element) {
+	var iteration int
+	var xy, p FloatPoint
+
+	window := fractal.Canvas.calculateWindow()
+
+	for i := 0; i < fractal.Canvas.Size.Width; i++ {
+		for j := 0; j < fractal.Canvas.Size.Height; j++ {
+			iteration = 0
+
+			xy = fractal.Canvas.scaleToWindow(i, j, window)
+			p = xy
+
+			for (p.X*p.X)+(p.Y*p.Y) < fractal.EscapeRadius*fractal.EscapeRadius && iteration <= fractal.MaxIterations {
+				p = FloatPoint{
+					X: (p.X * p.X) - (p.Y * p.Y) + xy.X,
+					Y: float32(math.Abs(float64(2*p.X*p.Y))) + xy.Y,
+				}
+
+				iteration++
+			}
+
+			items <- Element{i, j, float32(iteration) / float32(fractal.MaxIterations)}
+		}
+	}
+
+	close(items)
+}
