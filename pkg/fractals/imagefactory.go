219b62565a0adebe8c208ea08af3e364eacd1b3c
Andriy Astakhov <andrey.astakhov@gmail.com>
2026-04-08 16:09:12 +0200

    Improve readability of imagefactory

--- pkg/fractals/imagefactory.go
+++ pkg/fractals/imagefactory.go
@@ -14,20 +14,25 @@ type ImageFactory struct {
 }
 
 func (f ImageFactory) FromItems(items *FractalElements) *image.RGBA {
-	image := image.NewRGBA(image.Rect(0, 0, f.Width, f.Height))
+	img := image.NewRGBA(image.Rect(0, 0, f.Width, f.Height))
 
 	for _, item := range items.Elements {
 		paletteColour := f.Picker.Pick(int(1000 * item.Value))
-		colour := color.RGBA{uint8(paletteColour.R), uint8(paletteColour.G), uint8(paletteColour.B), 255}
+		colour := color.RGBA{
+			R: uint8(paletteColour.R),
+			G: uint8(paletteColour.G),
+			B: uint8(paletteColour.B),
+			A: 255,
+		}
 
-		image.Set(item.X, item.Y, colour)
+		img.Set(item.X, item.Y, colour)
 	}
 
 	if f.WithCenter {
-		drawCenter(*image)
+		drawCenter(*img)
 	}
 
-	return image
+	return img
 }
 
 func drawCenter(img image.RGBA) {
@@ -35,10 +40,11 @@ func drawCenter(img image.RGBA) {
 	c1 := (img.Bounds().Max.X - img.Bounds().Min.X) / 2
 	c2 := (img.Bounds().Max.Y - img.Bounds().Min.Y) / 2
 
-	img.Set(c1, c2, color.RGBA{255, 0, 0, 255})
+	centerColour := color.RGBA{R: 255, A: 255}
+	img.Set(c1, c2, centerColour)
 
-	img.Set(c1-1, c2-1, color.RGBA{255, 0, 0, 255})
-	img.Set(c1-1, c2+1, color.RGBA{255, 0, 0, 255})
-	img.Set(c1+1, c2-1, color.RGBA{255, 0, 0, 255})
-	img.Set(c1+1, c2+1, color.RGBA{255, 0, 0, 255})
+	img.Set(c1-1, c2-1, centerColour)
+	img.Set(c1-1, c2+1, centerColour)
+	img.Set(c1+1, c2-1, centerColour)
+	img.Set(c1+1, c2+1, centerColour)
 }
